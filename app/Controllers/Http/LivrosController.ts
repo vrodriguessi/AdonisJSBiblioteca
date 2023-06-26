57140e9c8c70ea28e4521cf7b64d940b253455c7
Veronica Rodrigues <veronicarodri2001@outlook.com>
2023-06-26 09:53:51 -0300

    Projeto Adonis, sistema de biblioteca

---
+++ app/Controllers/Http/LivrosController.ts
@@ -0,0 +1,10 @@
+// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class LivrosController {
+
+    public async store(){
+        return{
+        msg: 'Livro cadastrado com sucesso!'
+    }
+    }
+}

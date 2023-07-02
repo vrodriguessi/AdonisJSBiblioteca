f6d023639f7fa4bab486981e455df405d4ab178f
Veronica Rodrigues <veronicarodri2001@outlook.com>
2023-07-02 18:53:16 -0300

    Imagem 2

--- app/Controllers/Http/PessoasController.ts
+++ app/Controllers/Http/PessoasController.ts
@@ -1,12 +1,33 @@
+import { v4 as uuidv4} from 'uuid'
+
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 import Pessoa from 'App/Models/Pessoa'
 
+import  Application  from "@ioc:Adonis/Core/Application"
+
 export default class PessoasController {
+    private validationOptions = {
+        type: ['image'],
+        size: '2mb',
+    }
 
     public async store({request, response}:HttpContextContract){
         
         const body = request.body()
+
+        const image = request.file('image', this.validationOptions)
+
+        if(image){
+            const imageName = `${uuidv4()}.${image.extname}`
+
+            await image.move(Application.tmpPath('uploads'), {
+                name: imageName,
+            })
+
+            body.image = imageName
+
+        }
 
         const pessoa = await Pessoa.create(body)
 
